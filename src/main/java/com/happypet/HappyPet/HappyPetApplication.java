66580220ddb15a27ef4a5e11cad76d311934a09f
Daniel Mendes <danielmendes@MacBook-Pro-de-Daniel.local>
2021-04-07 16:55:39 +0100

    :tada: Start and configurate the project

---
+++ src/main/java/com/happypet/HappyPet/HappyPetApplication.java
@@ -0,0 +1,13 @@
+package com.happypet.HappyPet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/happypet/HappyPet/HappyPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happypet.HappyPet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
